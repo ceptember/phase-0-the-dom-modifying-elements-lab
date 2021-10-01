7cf0fa007bd0841765f85207161214b971da7d31
Christy Perozzi <christy.perozzi@gmail.com>
2021-09-30 22:43:16 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+//remove the <main> with id 'main':
+/*'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME 
+is the champion" inside: */
+
+document.getElementById('main').remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Christy is the champion"
+document.body.append(newHeader);
